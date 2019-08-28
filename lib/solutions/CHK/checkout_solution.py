651d66c5147796c073e482cd518f673c6b7ad0ef
sbx_user1051 <sbx_user1051@169.254.168.245>
2019-08-28 12:21:43 +0000

    Wed Aug 28 12:21:43 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,12 +11,14 @@ def checkout(skus):
     cnt_of_items = Counter(list_of_items)
 
     for item in cnt_of_items:
-        if (item in dictionary_of_deals) and (cnt_of_items[item] % dictionary_of_deals[item] == 0):
-            checkout_value += dictionary_of_deals_costs[item] * (cnt_of_items[item] / dictionary_of_deals[item])
+        if item in dictionary_of_deals:
+            diff = cnt_of_items[item] % dictionary_of_deals[item] # the diff using modulo
+            divisible = cnt_of_items[item] // dictionary_of_deals[item] # no remainder
+            checkout_value += (dictionary_of_costs[item] * diff) + (dictionary_of_deals_costs[item] * divisible)
         elif item not in dictionary_of_costs:
-          checkout_value = -1
-          break
+            return -1
         else:
             checkout_value += dictionary_of_costs[item] * cnt_of_items[item]
 
     return int(checkout_value)
+
