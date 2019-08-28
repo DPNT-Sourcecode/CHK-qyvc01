8d4a53d470fe89d2ad238bda777dfae4618943c5
sbx_user1051 <sbx_user1051@169.254.42.37>
2019-08-28 11:25:53 +0000

    Wed Aug 28 11:25:53 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,10 @@
+from collections import Counter
+
 def checkout(skus):
+    checkoutValue = 0
+    dictionaryOfCosts = {"A": 50, "B": 30, "C": 20, "D": 15}
     listOfItems = list(skus)
     cntOfItems = Counter(listOfItems)
+
 
 
