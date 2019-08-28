c3ddf547f647d48c02d4eb488739b1585f4aa74d
sbx_user1051 <sbx_user1051@169.254.42.37>
2019-08-28 11:49:57 +0000

    Wed Aug 28 11:49:57 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,8 +14,11 @@ def checkout(skus):
     for item in cnt_of_items:
         if (item in dictionary_of_deals) and (cnt_of_items[item] % dictionary_of_deals[item] == 0):
             checkout_value += dictionary_of_deals_costs[item] * (cnt_of_items[item] / dictionary_of_deals[item])
+        elif item not in dictionary_of_costs:
+            checkout_value += 0
         else:
             checkout_value += dictionary_of_costs[item] * cnt_of_items[item]
 
     return int(checkout_value)
+
 
