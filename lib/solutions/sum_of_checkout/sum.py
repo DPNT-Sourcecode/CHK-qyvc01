1eab443037ba084ece054783957ee96cf422e98d
sbx_user1051 <sbx_user1051@169.254.40.165>
2019-08-30 10:04:30 +0000

    Fri Aug 30 10:04:30 UTC 2019

--- lib/solutions/sum_of_checkout/sum.py
+++ lib/solutions/sum_of_checkout/sum.py
@@ -1,29 +1,28 @@
 def checkout_values(counter_of_items, dict_of_deals):
 
-    value = 0
 
     # loops through trying to find best deal on items
     # does this by subtracting max until it can do no more, pops the max and
     # loops with second max and so on...
+    value = 0
     for item in counter_of_items:
         if item in dict_of_deals:
-            num = counter_of_items[item]
-            list_of_items = list(dict_of_deals[item])
-            result = []
-
-            while num > 0:
-                num -= max(list_of_items)
-                if num == 0:
-                    result.append(max(list_of_items))
+            n = counter_of_items[item]
+            a = list(dict_of_deals[item])
+            ans = []
+            while n > 0:
+                n -= max(a)
+                if n == 0:
+                    ans.append(max(a))
                     break
-                if num > 0:
-                    result.append(max(list_of_items))
-                if num < 0:
-                    num += max(list_of_items)
-                    list_of_items.pop(list_of_items.index(max(list_of_items)))
-
-        for x in result:
+                if n > 0:
+                    ans.append(max(a))
+                if n < 0:
+                    n += max(a)
+                    a.pop(a.index(max(a)))
+        for x in ans:
             value += dict_of_deals[item][x]
 
-        return value
+    return value
+
 
