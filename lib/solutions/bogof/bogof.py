5fe950f2c667bc6f5b808b632ee0d4c01fc7550d
sbx_user1051 <sbx_user1051@169.254.40.165>
2019-08-30 09:49:30 +0000

    Fri Aug 30 09:49:30 UTC 2019

--- lib/solutions/bogof/bogof.py
+++ lib/solutions/bogof/bogof.py
@@ -6,3 +6,8 @@ def bogof(counter_of_items, dict_of_bogof, dict_of_deals):
                     how_many_bogofs = counter_of_items[item] // x
 
                     counter_of_items[[dict_of_bogof[item][x]] = counter_of_items[dict_of_bogof[item][x]] - how_many_bogofs
+
+    counter_of_items = +counter_of_items
+
+    return counter_of_items
+
